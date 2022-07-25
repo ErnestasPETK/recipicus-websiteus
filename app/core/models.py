cdae1c6dc55b21078ce215abfd0ce9623ccccc1e
ErnestasPETK <ernestas.petkevicius53@gmail.com>
2022-07-25 13:45:56 +0300

    test commit

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,71 @@
-from django.db import models  # noqa
+"""
+Database models.    
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
+
+class UserManager(BaseUserManager):
+    """
+    Custom user manager to deal with users in the system.
+    """
+
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Create, save and return a new user.
+        """
+        if not email:
+            raise ValueError("Users must have an email address.")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """
+        Create, save and returna a new superuser.
+        """
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    Custom user in the system.
+    """
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+
+    objects = UserManager()
+
+    # def __str__(self):
+    #     return self.email
+
+    # def get_full_name(self):
+    #     return self.name
+
+    # def get_short_name(self):
+    #     return self.name
+
+    # def has_perm(self, perm, obj=None):
+    #     return True
+
+    # def has_module_perms(self, app_label):
+    #     return True
+
+    # @property
+    # def is_superuser(self):
+    #     return self.is_staff
